45f81eb7ced772f892145cc5e516c15eea8f28f7
GabLeal <gabriel.leal@compasso.com.br>
2022-09-25 23:05:48 -0300

    feat: inicializando view de Intro através da view controller

--- appviewcode/Sources/Intro/IntroViewController.swift
+++ appviewcode/Sources/Intro/IntroViewController.swift
@@ -8,14 +8,26 @@
 import UIKit
 
 class IntroViewController: UIViewController {
+    
+    private var customView: IntroView? = nil
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        
-        self.view.backgroundColor = .red
+        buildView()
     }
 
+    private func buildView(){
+        view = IntroView()
+        customView = view as? IntroView
+        
+        customView?.delegate = self
+        
+    }
 
 }
 
+extension IntroViewController: SignWithGoogleDelegate {
+    func signin() {
+        self.navigationController?.pushViewController(LoginViewController(), animated: true)
+    }
+}
